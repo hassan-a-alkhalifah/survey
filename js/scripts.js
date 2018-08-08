2862b968fbd2e3bf45340b822d26eb36540e14b6
Colin Cook and Hassan Al-khalifah <colin@>
2018-08-08 16:31:53 -0700

    add js to print selected options on html DOM.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
+  $("form#survey").submit(function(event) {
   event.preventDefault();
   var food = $("#food").val();
   var wine = $("input:radio[name=wine]:checked").val();
-
+  var name = $("#name-input").val();
+  $("#food1").text(food);
+  $("#wine").text(wine);
+  $("#name").text(name);
+  });
 });
